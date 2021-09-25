c2354f5aba35cd355b80d2f8d9d756bea8f5eea0
tzDel <JuhlisSap123>
2021-09-25 13:41:05 +0200

    Spring Boot project creation

---
+++ src/main/kotlin/com/tzDel/Schmaggo/SchmaggoApplication.kt
@@ -0,0 +1,11 @@
+package com.tzDel.Schmaggo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SchmaggoApplication
+
+fun main(args: Array<String>) {
+	runApplication<SchmaggoApplication>(*args)
+}
