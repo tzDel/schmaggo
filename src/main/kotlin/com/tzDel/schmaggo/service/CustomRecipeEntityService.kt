67b4f12ed01a5728bb4787d943f8c6a0db04934b
tzDel <JuhlisSap123>
2021-11-13 22:20:41 +0100

    add logger to CustomRecipeService & CustomRecipeEntityService

--- src/main/kotlin/com/tzDel/schmaggo/service/CustomRecipeEntityService.kt
+++ src/main/kotlin/com/tzDel/schmaggo/service/CustomRecipeEntityService.kt
@@ -6,6 +6,7 @@ import com.tzDel.schmaggo.exception.RecipeDeletionException
 import com.tzDel.schmaggo.exception.RecipeNotFoundException
 import com.tzDel.schmaggo.exception.RecipePersistenceException
 import com.tzDel.schmaggo.model.CustomRecipe
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 import java.lang.IllegalArgumentException
@@ -15,32 +16,43 @@ import javax.persistence.EntityNotFoundException
 class CustomRecipeEntityService(
     @Autowired private val customRecipeRepository: ICustomRecipeRepository
 ) {
+    private val logger = LoggerFactory.getLogger(CustomRecipeService::class.java)
+
     fun tryGetCustomRecipeById(id: Int): CustomRecipe = try {
+        logger.info("Fetching custom recipe by ID $id ...")
         customRecipeRepository.getById(id)
     } catch (ex: EntityNotFoundException) {
+        logger.info("Fetching custom recipe by id $id failed")
         throw RecipeNotFoundException(ex)
     }
     
     fun tryGetCustomRecipesByName(recipeName: String): List<CustomRecipe?> = try {
+        logger.info("Fetching custom recipe by name $recipeName ...")
         customRecipeRepository.getCustomRecipesByName(recipeName)
     } catch (ex: IllegalArgumentException) {
+        logger.info("Fetching custom recipe by name $recipeName failed")
         throw RecipeNotFoundException(ex)
     }
     
     fun tryPersistCustomRecipe(recipeToPersist: CustomRecipe) = try {
+        logger.info("Start persisting recipe ${recipeToPersist.name} with id ${recipeToPersist.id} failed")
         customRecipeRepository.save(recipeToPersist)
     } catch (ex: IllegalArgumentException) {
+        logger.info("Persisting custom recipe ${recipeToPersist.name} with id ${recipeToPersist.id} failed")
         throw RecipePersistenceException(ex)
     }
     
     fun tryDeleteCustomRecipeById(id: Int) = try {
+        logger.info("Try deleting recipe with id $id")
         customRecipeRepository.deleteById(id)
     } catch (ex: IllegalArgumentException) {
+        logger.info("Deleting recipe with id $id failed")
         throw RecipeDeletionException(ex)
     }
 
     fun assertRecipeToAddIsUnique(recipeToAdd: CustomRecipe) {
         if (isAlreadyExisting(recipeToAdd)) {
+            logger.info("Recipe ${recipeToAdd.name} already exists")
             throw RecipeAlreadyExistingException()
         }
     }
--- src/main/kotlin/com/tzDel/schmaggo/service/CustomRecipeService.kt
+++ src/main/kotlin/com/tzDel/schmaggo/service/CustomRecipeService.kt
@@ -1,6 +1,7 @@
 package com.tzDel.schmaggo.service
 
 import com.tzDel.schmaggo.model.CustomRecipe
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 
@@ -8,12 +9,14 @@ import org.springframework.stereotype.Service
 class CustomRecipeService(
     @Autowired private val customRecipeEntityService: CustomRecipeEntityService
 ): ICustomRecipeService {
+    private val logger = LoggerFactory.getLogger(CustomRecipeService::class.java)
 
     override fun getAllCustomRecipes(): List<CustomRecipe?> = customRecipeEntityService.getAllCustomRecipes()
-
     override fun deleteCustomRecipeId(id: Int) = customRecipeEntityService.tryDeleteCustomRecipeById(id)
 
     override fun updateCustomRecipe(id: Int, recipe: CustomRecipe) {
+        logger.info("Start updating recipe ${recipe.name} with ID: $id")
+
         val updatedRecipe = customRecipeEntityService
             .tryGetCustomRecipeById(id)
             .apply {
@@ -27,6 +30,7 @@ class CustomRecipeService(
     }
 
     override fun addCustomRecipe(recipeToAdd: CustomRecipe) {
+        logger.info("Start adding recipe ${recipeToAdd.name}")
         customRecipeEntityService.assertRecipeToAddIsUnique(recipeToAdd)
         customRecipeEntityService.tryPersistCustomRecipe(recipeToAdd)
     }
